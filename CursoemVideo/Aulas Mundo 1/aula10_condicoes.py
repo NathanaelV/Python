4ad4b120ef36d2a048b4ae0c7edd598a307a82a7
NathanaelV <tmntsplinter04@gmail.com>
2021-04-13 13:32:06 -0300

    Update aula 10
    Feito até o min 21'22"
    Continua...

---
+++ CursoemVideo/Aulas Mundo 1/aula10_condicoes.py
@@ -0,0 +1,14 @@
+# Challenge 028 - 035
+
+# Sabendo se o carro é velho ou não
+tempo = int(input('Quantos anos tem seu carro?\n'))
+if tempo <= 3:
+    print('Carro novo.')
+else:
+    print('Carro velho.')
+print('---FIM---')
+
+# Condição simplificada
+print('CARRO NOVO' if tempo <= 3 else 'CARRO VELHO')
+
+# Parte Prática:
