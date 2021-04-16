974049a737d5bebae1d870f5495a7c953265c4a1
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 18:25:04 -0300

    Update ex 54

--- CursoemVideo/Exercicios Mundo 2/ex053_detector_de_palindromo.py
+++ CursoemVideo/Exercicios Mundo 2/ex053_detector_de_palindromo.py
@@ -6,4 +6,17 @@
 # Ex.: Apos a sopa; a sacada da casa; a torre da derrota; o lobo ama o bolo;
 # Anotaram a data da maratona.
 # Desconsiderar os espaços e acentos. Programa tira os espaços
+
 print('Descobrindo se a frase é um Palindromo.')
+frase = str(input('Digite uma frase: ')).strip().upper()
+array = frase.split()
+junto = ''.join(array)
+n = len(junto)
+s = 0
+for a in range(1, n+1):
+    if junto[a-1] != junto[n-a]:
+        s += 1
+if s == 0:
+    print('A frase é um Palindromo.')
+else:
+    print('A frase não é um Palindromo')
