0eea585fccc44a2382ac0e1210fb7b5922d53136
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 16:39:18 -0300

    Update ex 46
    minha versão

--- CursoemVideo/Exercicios Mundo 2/ex046_contagem_regressiva.py
+++ CursoemVideo/Exercicios Mundo 2/ex046_contagem_regressiva.py
@@ -2,3 +2,8 @@
 
 # Mostrar uma contagem regressiva de 10 até 0
 # Com pausa de 1 segundo entre eles
+from time import sleep
+for a in range(10, 0, -1):
+    print(a)
+    sleep(1)
+print('Booommm!!! Feliz ano novo!!!')
