fcbbb1fa53b599c92b6f18a4945f8aa8be69fe40
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 11:26:16 -0300

    Update ex042_analisando_triangulos_v2.py

--- CursoemVideo/Exercicios Mundo 2/ex042_analisando_triangulos_v2.py
+++ CursoemVideo/Exercicios Mundo 2/ex042_analisando_triangulos_v2.py
@@ -9,14 +9,25 @@ r1 = int(input('Digite o primeiro valor de um segmento de reta: '))
 r2 = int(input('Digite o segundo valro de um segmento de reta: '))
 r3 = int(input('Digite o terceiro valor de um segmento de reta: '))
 
-if r1 + r2 <= r3 or r1 + r3 <= r2 or r2 + r3 <= r1:
-    print('Não é possível formar um triângulo.')
-if r1 + r2 > r3 or r1 + r3 > r2 or r2 + r3 > r1:
-    if r1 == r2 and r1 == r3:
-        print('É possível formar um triângulo equilátero.')
+if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
+    if r1 == r2 == r3:  # Python permite essa syntaxe
+        print('É possível formar um TRIÂNGULO EQUILATERO.')
     elif r1 == r2 or r1 == r3 or r2 == r3:
-        print('É possível formar um triângulo isóceles.')
+        print('É possível formar um TRIÂNGULO ISÓSCELES.')
     else:
-        print('É possível formar um triângulo escaleno.')
+        print('É possível formar um TRIANGULO ESCALENO.')
 else:
-    print('2 Não é possível formar um triângulo')
+    print('NÃO é possível formar um TRIÂNGULO')
+
+# Exemplo do professor
+print('\nExemplo do professor:')
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR um triângulo ', end='')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO!')
+    elif r1 != r2 != r3 != r1:  # Caso não colocque r3 != r1, vai permitir que r1 = r3
+        print('ESCALENO!')
+    else:
+        print('ISÓSCELES')
+else:
+    print('Não é possível formar um triângulo.')
