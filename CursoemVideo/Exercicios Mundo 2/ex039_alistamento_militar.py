7c1bc3b9024d128e4fb931fc2c6022f7c4954e54
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 10:13:56 -0300

    Atualização ex 39

--- CursoemVideo/Exercicios Mundo 2/ex039_alistamento_militar.py
+++ CursoemVideo/Exercicios Mundo 2/ex039_alistamento_militar.py
@@ -1,4 +1,4 @@
-# Class Challenge
+# Class Challenge 12
 # Entrar com o ano de nascimento e calcular a idade e devolver:
 # Informar quanto tempo falta falta para se alistar no exército,
 # Se é o ano ou se já passou, quanto tempo passou.
@@ -8,8 +8,11 @@ idade = date.today().year - nascimento
 if idade < 18:
     tempo = 18 - idade
     print('Faltam {} anos para o seu alistamento no exército'.format(tempo))
+    print('Seu alistamento será em {}'.format(nascimento + 18))
 elif idade > 18:
     tempo = idade - 18
-    print('O ano do seu alistamento no exército foi há {} anos'.format(tempo))
+    print('O ano do seu alistamento no exército foi há {} anos, em {}'.format(tempo, nascimento + 18))
 else:
     print('Esse é o ano do seu alistamento no exército, não esqueça os documentos.')
+
+# Exemplo do professor: Muito semelhante
--- CursoemVideo/Exercicios Mundo 2/ex045_game_pedra_papel_tesoura.py
+++ CursoemVideo/Exercicios Mundo 2/ex045_game_pedra_papel_tesoura.py
@@ -3,7 +3,9 @@
 # Criar um programa que jogue Jokenpô
 print('Vamos jogar Jokenpô')
 from random import randint
-computador = randint(4, 6) # 4 = Pedra, 5 = Papel, 6 = Tesoura
+computador = randint(0, 2) # 0 = Pedra, 1 = Papel, 2 = Tesoura
+jogo = ['pedra', 'papel', 'tesoura']
+print(jogo[computador])
 print(computador)
-jogador = str(input('Digite sua escolha. Pedra, papel ou tesoura'))
-if jogador.lower() == 'pedra':
+# jogador = str(input('Digite sua escolha. Pedra, papel ou tesoura'))
+# if jogador.lower() == 'pedra':
