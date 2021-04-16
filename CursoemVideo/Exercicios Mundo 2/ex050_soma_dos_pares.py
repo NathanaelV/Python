4bad66cc478f0e47505e62a07aaab506af5f62a9
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 17:11:07 -0300

    Update ex50

--- CursoemVideo/Exercicios Mundo 2/ex050_soma_dos_pares.py
+++ CursoemVideo/Exercicios Mundo 2/ex050_soma_dos_pares.py
@@ -2,5 +2,13 @@
 
 # Ler 6 números inteiros e só vai somar se for par.
 # Números impares serão ignorados.
-for a in range(0,6):
+s = 0
+i = 0
+for a in range(0, 6):
     n = int(input('Digite um número: '))
+    if n % 2 == 0:
+        s += n
+    else:
+        i += n
+print('Soma dos pares =', s)
+print('Soma dos ímpares', i)
