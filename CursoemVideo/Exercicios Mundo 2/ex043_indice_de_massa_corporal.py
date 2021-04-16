86b209c8d317382ce9bba92d9025aa6494722670
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 11:49:18 -0300

    Update

--- CursoemVideo/Exercicios Mundo 2/ex043_indice_de_massa_corporal.py
+++ CursoemVideo/Exercicios Mundo 2/ex043_indice_de_massa_corporal.py
@@ -7,8 +7,8 @@
 # Entre 30 e 40: Obesidade
 # Acima de 40: Obesidade mórbida
 print('Vamos calcular o seu IMC?')
-h = float(input('Insira sua altura em metros: '))
-m = float(input('Qual o seu peso? Não vale mentir: '))
+h = float(input('Insira sua altura? (m): '))
+m = float(input('Qual o seu peso? (Kg)): '))
 IMC = m / h**2
 if IMC < 18.49:
     print('Seu IMC é {:.1f}. Você está abaixo do peso.\nProcure um médico.'.format(IMC))
@@ -20,3 +20,18 @@ elif IMC < 40:
     print('Seu IMC é {:.1f}. Você está Obeso.\nProcure um médico rápido.'.format(IMC))
 else:
     print('Seu IMC é {:.1f}. Você está com Obesidade Mórbida.\nProcure um médico Urgentemente!!!'.format(IMC))
+
+# Exemplo do professor:
+
+print('\nExemplo do professor:')
+print('O seu IMC é de {:.1f}'.format(IMC))
+if IMC < 18.49:
+    print('Você está abaixo do peso.')
+elif IMC < 25:
+    print('Peso normal')
+elif IMC < 30:
+    print('Você está com Sobrepeso')
+elif IMC < 40:
+    print('Você está Obeso')
+else:
+    print('Você está com Obesidade Mórbida')
