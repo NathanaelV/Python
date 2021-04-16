b30212757690df81cd28ac3d8e054454a37500bf
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 19:15:06 -0300

    Update 55 e 54

--- CursoemVideo/Exercicios Mundo 2/ex055_maior_e_menor_da_sequencia.py
+++ CursoemVideo/Exercicios Mundo 2/ex055_maior_e_menor_da_sequencia.py
@@ -1,11 +1,17 @@
 # Class Challenge 13
 
 # Ler o peso de 5 pessas e verificar qual é maior e o menor
+s = 0
 maior = 0
 menor = 100000
 for c in range(0, 5):
-    m = float(input('Digite um peso: (Kg)'))
+    m = float(input('Digite um peso: (Kg) '))
+    s += m
     if m > maior:
         maior = m
     if m < menor:
         menor = m
+media = s / 5
+print('O mais pesado pesa {}'.format(maior))
+print('O mais leve pesa {}'.format(menor))
+print('A média dos pesos é {:.1f}'.format(media))
--- CursoemVideo/Exercicios Mundo 2/ex056_analisador_completo.py
+++ CursoemVideo/Exercicios Mundo 2/ex056_analisador_completo.py
@@ -1,5 +1,25 @@
-# Class Challenge
+# Class Challenge 13
 
 # Ler Nome, idade e sexo de 4 pessoas. E Mostrar
 # A média das idades, nome do homem mais velho,
 # Quantas mulheres com menos de 20 anos
+
+i = 0  # Para o cálculo da somatória das idades
+n = 0  # Para saber o nome do mais velho
+f = 0  # Contar o número de mulheres com menos de 20 anos
+nomevelho = 'João'
+
+for c in range(0, 4):
+    nome = str(input('Digite o nome: '))
+    idade = int(input('Digite a idade: '))
+    sex = str(input('Digite o sexo [F/M]: ')).strip().upper()
+    i += idade
+    if sex == 'M' and idade > n:
+        nomevelho = nome
+        n = idade
+    if sex == 'F' and idade < 20:
+        f += 1
+
+print('\nA média das idades é {:.0f}.'.format(i/4))
+print('O homem mais velho é o {}.'.format(nomevelho))
+print('O grupo tem {} mulher(es) com menos de 20 anos.'.format(f))
