d2e43cb0493ada4672bdd1f991e4ada7b9f49bde
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 09:41:00 -0300

    Update ex037_conversor_de_bases_numericas.py

--- CursoemVideo/Exercicios Mundo 2/ex037_conversor_de_bases_numericas.py
+++ CursoemVideo/Exercicios Mundo 2/ex037_conversor_de_bases_numericas.py
@@ -3,14 +3,14 @@
 # Digitar um número e fazer a conversão para uma das opções:
 # 1 - Binário; 2 - Octal; 3 - Hexadecimal
 print('\033[1;32mEsse programa converte um número inteiro em uma das bases:\n'
-      '\033[1;35m1 - Binário\n\033[1;36m2 - Octal\n\033[1;32m3 - Hexadecimal\033[m')
+      '\033[1;35m    1 - Binário\n\033[1;36m    2 - Octal\n\033[1;32m    3 - Hexadecimal\033[m')
 base = int(input('Deseja converter um número para qual base?: '))
 número = int(input('Digite o número que deseja converte: '))
 if base == 1:
-      print('O número {} convertido para Binário é:\n{}'.format(número, bin(número)))
+      print('O número {} convertido para Binário é:\n{}'.format(número, bin(número)[2:]))
 elif base == 2:
-      print('O número {} convertido para Octal é:\n{}'.format(número, oct(número)))
+      print('O número {} convertido para Octal é:\n{}'.format(número, oct(número)[2:]))
 elif base == 3:
-      print('O número {} convertido para Hexadecimal é:\n{}'.format(número, hex(número)))
+      print('O número {} convertido para Hexadecimal é:\n{}'.format(número, hex(número)[2:]))
 else:
       print('Por favor, digite uma opção válida. 1, 2 ou 3.')
