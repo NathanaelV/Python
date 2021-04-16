ded5a67a3d851a05b2217fdd5b3e8bdeba65f0bc
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 18:40:25 -0300

    Update ex 54

--- CursoemVideo/Exercicios Mundo 2/ex054_grupo_da_maioridade.py
+++ CursoemVideo/Exercicios Mundo 2/ex054_grupo_da_maioridade.py
@@ -1,4 +1,13 @@
 # Class Challenge 13
 
-# Ler o ano de nascimento de 7 pessoas e verificar quantos já atingiram os 18 anos
-# Mostrar quantos tem 18 ou 21 ou mais e quantos não
+# Ler o ano de nascimento de 7 pessoas e verificar quantos já atingiram os 18 anos.
+# Mostrar quantos temm 18 ou 21, ou mais e quantos ainda não completaram.
+from datetime import date
+
+s = 0
+for c in range(0, 7):
+    nascimente = int(input('Digite a data de nascimento: '))
+    idade = date.today().year - nascimente
+    if idade >= 18:
+        s += 1
+print('No grupo têm {} pessoa(s) que chegaram na maioridade.'.format(s))
--- CursoemVideo/Exercicios Mundo 2/ex055_maior_e_menor_da_sequencia.py
+++ CursoemVideo/Exercicios Mundo 2/ex055_maior_e_menor_da_sequencia.py
@@ -1,3 +1,11 @@
 # Class Challenge 13
 
-# Ler o peso de 5 pessas e verificar qual é maior e menor
+# Ler o peso de 5 pessas e verificar qual é maior e o menor
+maior = 0
+menor = 100000
+for c in range(0, 5):
+    m = float(input('Digite um peso: (Kg)'))
+    if m > maior:
+        maior = m
+    if m < menor:
+        menor = m
