f5a8f390a29a8e950235b95aef32ff9c6102a004
NathanaelV <tmntsplinter04@gmail.com>
2021-04-16 16:56:20 -0300

    Update ex 48
    E

--- CursoemVideo/Exercicios Mundo 2/ex048_soma_impares_multiplos_de_tres.py
+++ CursoemVideo/Exercicios Mundo 2/ex048_soma_impares_multiplos_de_tres.py
@@ -2,3 +2,10 @@
 
 # Soma de todos os números ímpares, multiplos de 3 entre 1 e 500
 # Fazer também um exemplo usando if else
+s = 0
+for a in range(0, 51):
+    if a % 2 == 1 and a % 3 == 0:
+        print(a)
+        s += a
+print('Soma =', s)
+print('Fim')
--- CursoemVideo/Exercicios Mundo 2/ex049_tabuada_v2.py
+++ CursoemVideo/Exercicios Mundo 2/ex049_tabuada_v2.py
@@ -1,3 +1,6 @@
 # Class Challenge 13
 
 # Fazer uma tabuada.
+print('Fazendo Tabuada!')
+t = int(input('Deseja saber a tabuada de qual número? '))
+print('-' * 15)
