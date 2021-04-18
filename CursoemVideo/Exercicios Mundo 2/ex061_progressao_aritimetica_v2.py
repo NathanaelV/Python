93da9bed71740e1cfdeaefe003dc51415f66b616
NathanaelV <tmntsplinter04@gmail.com>
2021-04-18 19:54:59 -0300

    Update ex 61

--- CursoemVideo/Exercicios Mundo 2/ex061_progressao_aritimetica_v2.py
+++ CursoemVideo/Exercicios Mundo 2/ex061_progressao_aritimetica_v2.py
@@ -1,3 +1,12 @@
 # Class Challenge 14
 
 # Ler o primeiro termo e a razão de uma PA. Mostrar os 10 primeiros números
+
+primeiro = int(input('Digite o 1º termo: '))
+razao = int(input('Digite a razão: '))
+i = 0
+while i < 10:
+    print(primeiro, end=', ')
+    primeiro += razao
+    i += 1
+print('Fim!')
