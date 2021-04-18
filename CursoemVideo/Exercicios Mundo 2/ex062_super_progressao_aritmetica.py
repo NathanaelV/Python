fded7964640053c436c4647a400f9cf2dd488ef6
NathanaelV <tmntsplinter04@gmail.com>
2021-04-18 20:29:05 -0300

    Update ex 62 and 63

--- CursoemVideo/Exercicios Mundo 2/ex062_super_progressao_aritmetica.py
+++ CursoemVideo/Exercicios Mundo 2/ex062_super_progressao_aritmetica.py
@@ -2,3 +2,15 @@
 
 # Fazer o anterior, porém quando terminar, perguntat se quer saber mais termos
 # Mostrar quantos termos o usuario está pedindo até o usuário digitar 0 (zero)
+
+first = int(input('Digite o primeiro termo: '))
+difference = int(input('Digite a razão: '))
+i = 1
+while i < 11:
+    print(first, end=', ')
+    first += difference
+    i += 1
+    if i == 11:
+        n = int(input('\nDeseja saber mais quantos termos? '))
+        i -= n
+print('Fim!')
--- CursoemVideo/Exercicios Mundo 2/ex063_sequencia_de_fibonacci_v1.py
+++ CursoemVideo/Exercicios Mundo 2/ex063_sequencia_de_fibonacci_v1.py
@@ -2,3 +2,16 @@
 
 # Ler um número n e mostrar os n primeiros números de Fibonacci
 # 0, 1, 1, 2, 3, 5, 8...
+
+n = int(input('Digite enéssimo número que deseja saber da sequencia de Fibonacci: '))
+a1 = 0
+a2 = 1
+a3 = 1
+i = 0
+while i < n:
+    print(a3, end=', ')
+    a3 = a1 + a2
+    a2 = a1
+    a1 = a3
+    i += 1
+print('Fim!')
