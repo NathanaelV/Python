4abe8d361c31114049cd472ee53486ddcbb7231b
NathanaelV <tmntsplinter04@gmail.com>
2021-04-18 15:16:05 -0300

    Update ex 58

--- CursoemVideo/Exercicios Mundo 2/ex058_jogo_da_adivinhacao_v2.py
+++ CursoemVideo/Exercicios Mundo 2/ex058_jogo_da_adivinhacao_v2.py
@@ -2,3 +2,22 @@
 
 # Melhor o jogo do ex 28. Número entre 0 e 10.
 # Tentar até acertar, e contabilizar o número de tentativas
+
+from random import randint
+from time import sleep
+print('{:-^46}'.format(' JOGO DA ADIVINHAÇÃO 2.0 '))
+print('Consegue adivinhar o número que eu pensei??\nDica: É um número entre 0 e 10!')
+comp = randint(0, 10)
+i = 0
+n = 11
+print(comp)
+while n != comp:
+    n = int(input('Digite seu palpite: '))
+    i += 1
+    sleep(0.5)
+    if n != comp:
+        print('Não. Tente de novo!')
+        sleep(0.5)
+print('Parabén você acertou!!!')
+sleep(0.5)
+print('Levou apenas {} tentativa(s).'.format(i))
