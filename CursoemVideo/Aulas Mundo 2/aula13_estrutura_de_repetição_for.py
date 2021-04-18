aab2ba4061e0345312579e4dd897e876d17e75a0
NathanaelV <tmntsplinter04@gmail.com>
2021-04-18 13:46:16 -0300

    Update e criação
    Update ex 56 e criação da aula 14

--- CursoemVideo/Aulas Mundo 2/aula13_estrutura_de_repetição_for.py
+++ CursoemVideo/Aulas Mundo 2/aula13_estrutura_de_repetição_for.py
@@ -1,4 +1,5 @@
 # Challenge 46 - 56
+# Funciona bem quando sei até onde eu quero ir.
 
 for a in range(0, 6):  # Repetirá 5 vezes. Conta o 1 mas não conta o 6
     print('Hello')     # Ele para no 6
---
+++ CursoemVideo/Aulas Mundo 2/aula14_estrutura_de_repeticao_while.py
@@ -0,0 +1,1 @@
+# Challenge 57 - 65
