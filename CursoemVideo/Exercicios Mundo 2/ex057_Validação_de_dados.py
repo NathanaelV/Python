49a53bf4da35369a24b0ecd9e5834f880d2e5062
NathanaelV <tmntsplinter04@gmail.com>
2021-04-18 15:02:36 -0300

    Update ex 57

--- CursoemVideo/Exercicios Mundo 2/ex057_Validação_de_dados.py
+++ CursoemVideo/Exercicios Mundo 2/ex057_Validação_de_dados.py
@@ -3,6 +3,6 @@
 # Pedir a digitação do sexo de uma pessoa, M ou F, caso esteja errado, solicitar novamente
 
 r = ''
-while r != 'M' != 'F':
+while r != 'M' and r != 'F':
     r = str(input('Digite o seu sexo [M/F]: ')).strip().upper()
 print('Obrigado!')
