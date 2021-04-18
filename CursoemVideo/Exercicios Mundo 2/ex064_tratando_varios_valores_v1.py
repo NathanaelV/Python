bd93dea82d53e2371ccedcc842687ecd6eb88079
NathanaelV <tmntsplinter04@gmail.com>
2021-04-18 20:35:47 -0300

    Update ex 64

--- CursoemVideo/Exercicios Mundo 2/ex064_tratando_varios_valores_v1.py
+++ CursoemVideo/Exercicios Mundo 2/ex064_tratando_varios_valores_v1.py
@@ -2,3 +2,13 @@
 
 # Ler vários números inteiros até o usuario digitar 999.
 # Mostrar quantos foram digitados e mostrar a soma, disconciderando o 999
+
+cont = 0
+s = 0
+n = 0
+while n != 999:
+    n = int(input('Digite um número: '))
+    if n != 999:
+        cont += 1
+        s += n
+print('Foram digitados {} numeros e a soma é {}'.format(cont, s))
