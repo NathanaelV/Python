af5a68a88aa0a5fa501b5fed81ac428d81ff26d9
NathanaelV <tmntsplinter04@gmail.com>
2021-04-19 19:11:00 -0300

    Teacher exercises.

--- CursoemVideo/Exercicios Mundo 2/ex066_varios_numeros_com_flag.py
+++ CursoemVideo/Exercicios Mundo 2/ex066_varios_numeros_com_flag.py
@@ -5,11 +5,14 @@
 # Can't add output variable, 999.
 # I need to use f string
 
-c = s = n = 0
-while True:
+c = s = 0
+while True:  # Com o While True não preciso declarar n antes. Não faz gambiarra
     n = int(input('Enter a number [type 999 to exit]: '))
-    if n == 999:
+    if n == 999:  # Forma sem gambiarra.
         break
     s += n
     c += 1
 print(f'{c} number(s) were entered and its(their) sum is {s}.')
+
+# Teacher example:
+# The Teacher's example is similar
