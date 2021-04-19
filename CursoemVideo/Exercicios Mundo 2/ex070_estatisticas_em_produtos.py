d4d13ed79d43f7e5611e18d07b11175ad1873e9f
NathanaelV <tmntsplinter04@gmail.com>
2021-04-19 18:19:14 -0300

    Update ex 70

--- CursoemVideo/Exercicios Mundo 2/ex070_estatisticas_em_produtos.py
+++ CursoemVideo/Exercicios Mundo 2/ex070_estatisticas_em_produtos.py
@@ -4,3 +4,32 @@
 # In the end it should show:
 # What is the total spent. How many products cost more than R$ 1000.
 # Cheapest product name.
+
+s = 0
+over1000 = cheap = c = 0
+cheapest = ''
+while True:
+    product = str(input('What is the product name? '))
+    price = float(input('What is the product price? R$ '))
+    answer = str(input('There is another product? [Y/N] ')).strip().upper()[0]
+    while answer not in 'YN':
+        print('\nINVALID! Enter a valid answer.')
+        answer = str(input('There is another product? [Y/N] ')).strip().upper()[0]
+
+    c += 1
+    s += price
+    if c == 1:
+        cheap = price
+        cheapest = product
+    if price > 1000:
+        over1000 += 1
+    if cheap > price:
+        cheap = price
+        cheapest = product
+    if answer == 'N':
+        break
+print('=' * 50)
+print(f'The total spent is R$ {s}')
+print(f'There are {over1000} products over R$ 1000.00.')
+print(f'The cheapest product is the {cheapest.capitalize()} and coast R$ {cheap:.2f} ')
+print('=' * 50)
