3a22407315b94d12ba9ecdb9fbdc199331a97cc8
NathanaelV <tmntsplinter04@gmail.com>
2021-04-19 17:36:50 -0300

    Update ex 68 and 69

--- CursoemVideo/Exercicios Mundo 2/ex068_jogo_do_par_ou_impar.py
+++ CursoemVideo/Exercicios Mundo 2/ex068_jogo_do_par_ou_impar.py
@@ -3,9 +3,31 @@
 # Make a game to play 'par ou impar' (even or odd). The game will only stop if the player loses.
 # Show how many times the player has won.
 from random import randint
-comp = randint(1, 6)
-n = c = 0
-while c < 10:
-    print(comp, end=' ')
-    c += 1
 
+c = 0
+print('Lets play Par ou Impar')
+
+while True:
+    comp = randint(1, 6)
+    np = int(input('Enter a number: '))
+    rp = str(input('type a guess [P/I]: ')).strip().upper()[0]
+    while rp not in 'PpIi':
+        rp = str(input('Enter a valid guess [P/I]: ')).strip().upper()[0]
+
+    s = np + comp
+    if s % 2 == 0:
+        r = 'P'
+    else:
+        r = 'I'
+
+    if rp == r:
+        print('~~' * 20)
+        print('CONGRATULATIONS! You Win.')
+        print('Lets play again...')
+        print('~~' * 20)
+        c += 1
+    else:
+        print('The Computer win')
+        print(f'You won {c} matches in a row')
+        print('Try again.')
+        break
--- CursoemVideo/Exercicios Mundo 2/ex069_analise_de_dados_do_grupo.py
+++ CursoemVideo/Exercicios Mundo 2/ex069_analise_de_dados_do_grupo.py
@@ -5,3 +5,28 @@
 # In the end it should show:
 # How many are over 18. How many men have. How many women under 20.
 # Validate the information, only accept if the information is correct.
+
+older18 = men = women = 0
+while True:
+    age = int(input('Enter your age: '))
+    gender = str(input('Enter your gender [M/F]: ')).strip().upper()[0]
+    while gender not in 'MF':
+        gender = str(input('\nInvalid! Enter a valid gender [M/F]: ')).strip().upper()[0]
+
+    answer = str(input('Do you wish to continue? [Y/N] ')).strip().upper()[0]
+    while answer not in 'YN':
+        print('\nInvalid!!! Enter a valid alternative. ')
+        answer = str(input('Do you wish to continue? [Y/N] ')).strip().upper()[0]
+
+    if age > 18:
+        older18 += 1
+    if gender == 'M':
+        men += 1
+    if gender == 'F' and age < 20:
+        women += 1
+    if answer == 'N':
+        break
+
+print(f'There are {older18} people over 18 years old.')
+print(f'There are {men} men.')
+print(f'There are {women} women under 20 years old.')
