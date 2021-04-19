866255a68bde9b66ba5ca7525d068af3fefaf908
NathanaelV <tmntsplinter04@gmail.com>
2021-04-19 15:40:32 -0300

    Update ex 66

--- CursoemVideo/Exercicios Mundo 2/ex066_varios_numeros_com_flag.py
+++ CursoemVideo/Exercicios Mundo 2/ex066_varios_numeros_com_flag.py
@@ -1,6 +1,15 @@
-# Class CHallenge 15
+# Class Challenge 15
 
 # read several integers and only stop when typing 999
 # Show how many numbers were entered and what is the sum.
 # Can't add output variable, 999.
 # I need to use f string
+
+c = s = n = 0
+while True:
+    n = int(input('Enter a number [type 999 to exit]: '))
+    if n == 999:
+        break
+    s += n
+    c += 1
+print(f'{c} number(s) were entered and its(their) sum is {s}.')
