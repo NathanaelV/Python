d0cbbdc72bfe19037a2a01d12718d19bfe583874
NathanaelV <tmntsplinter04@gmail.com>
2021-04-19 16:05:52 -0300

    Update ex 67

--- CursoemVideo/Exercicios Mundo 2/ex067_Tabuada_v3.py
+++ CursoemVideo/Exercicios Mundo 2/ex067_Tabuada_v3.py
@@ -1,4 +1,16 @@
 # Class Challenge 15
 
 # Show a multiplication table, only stop when the user enters a negative number
-
+n = 0
+while True:
+    print('=' * 50)
+    print('{:^50}'.format('MULTIPLICATION TABLE'))
+    print('{:^50}'.format('Enter a negative number to EXIT!'))
+    print('=' * 50)
+    n = int(input('Enter a number to find your multiplication table: '))
+    c = 1
+    if n < 0:
+        break
+    while c <= 10:
+        print(f'{n} x {c:>2} = {n*c}')
+        c += 1
--- CursoemVideo/Exercicios Mundo 2/ex068_jogo_do_par_ou_impar.py
+++ CursoemVideo/Exercicios Mundo 2/ex068_jogo_do_par_ou_impar.py
@@ -2,4 +2,10 @@
 
 # Make a game to play 'par ou impar' (even or odd). The game will only stop if the player loses.
 # Show how many times the player has won.
+from random import randint
+comp = randint(1, 6)
+n = c = 0
+while c < 10:
+    print(comp, end=' ')
+    c += 1
 
