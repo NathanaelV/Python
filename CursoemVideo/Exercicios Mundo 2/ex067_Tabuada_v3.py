506f866e2bb9ec3389f64b9a7f4ff61377801d85
NathanaelV <tmntsplinter04@gmail.com>
2021-04-19 22:51:39 -0300

    Update teacher exercises

--- CursoemVideo/Exercicios Mundo 2/ex067_Tabuada_v3.py
+++ CursoemVideo/Exercicios Mundo 2/ex067_Tabuada_v3.py
@@ -1,7 +1,7 @@
 # Class Challenge 15
 
 # Show a multiplication table, only stop when the user enters a negative number
-n = 0
+
 while True:
     print('=' * 50)
     print('{:^50}'.format('MULTIPLICATION TABLE'))
@@ -14,3 +14,16 @@ while True:
     while c <= 10:
         print(f'{n} x {c:>2} = {n*c}')
         c += 1
+
+# Teacher's Example:
+
+print('\nTeacher Example: ')
+while True:
+    n = int(input('Quer ver a tabuada de Qual número? '))
+    if n < 0:
+        break
+    print('-' * 30)
+    for c in range(1, 11):
+        print(f'{n} x {c:>2} = {n*c}')
+    print('-' * 30)
+print('PROGRAMA DE TABUADA ENCERRADA. Volte Sempre')
--- CursoemVideo/Exercicios Mundo 2/ex068_jogo_do_par_ou_impar.py
+++ CursoemVideo/Exercicios Mundo 2/ex068_jogo_do_par_ou_impar.py
@@ -8,12 +8,12 @@ c = 0
 print('Lets play Par ou Impar')
 
 while True:
-    comp = randint(1, 6)
+    comp = randint(1, 4)
     np = int(input('Enter a number: '))
     rp = str(input('type a guess [P/I]: ')).strip().upper()[0]
     while rp not in 'PpIi':
         rp = str(input('Enter a valid guess [P/I]: ')).strip().upper()[0]
-
+    print(f'You played {np} and computer played {comp}')
     s = np + comp
     if s % 2 == 0:
         r = 'P'
@@ -27,7 +27,39 @@ while True:
         print('~~' * 20)
         c += 1
     else:
+        print('~~' * 20)
         print('The Computer win')
         print(f'You won {c} matches in a row')
         print('Try again.')
+        print('~~' * 20)
         break
+
+# Teacher Example:
+
+print('\nTeacher example: ')
+v = 0
+while True:
+    jogador = int(input('Digite um valor: '))
+    computador = randint(1, 4)
+    total = jogador + computador
+    tipo = ''
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total}', end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU ÍMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    print('Vamos Jogar Novamente...')
+print(f'GAME OVER. Você venceu {v} vez(es).')
