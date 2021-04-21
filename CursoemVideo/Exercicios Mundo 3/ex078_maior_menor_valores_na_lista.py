ea6ff13d6b855d6dc8d6348d705aea09037ca14a
NathanaelV <tmntsplinter04@gmail.com>
2021-04-21 13:32:23 -0300

    Exercises 78

--- CursoemVideo/Exercicios Mundo 3/ex078_maior_menor_valores_na_lista.py
+++ CursoemVideo/Exercicios Mundo 3/ex078_maior_menor_valores_na_lista.py
@@ -4,3 +4,18 @@
 # Mostrar qual o maior e qual o menor
 # Mostrar a posição do maior e do menor
 # Se aparecer mais de uma vez, deve mostrar todas as posições
+
+position = ('first', 'second', 'third', 'fourth', 'fifth')
+val = list()
+for c in range(0, 5):
+    val.append(int(input(f'Enter {position[c]} number: ')))
+
+print(f'The largest is {max(val)} and appears in the position: ', end='')
+for pmax, vmax in enumerate(val):
+    if vmax == max(val):
+        print(pmax+1, end='...')
+
+print(f'\nThe smallest is {min(val)} and appears in the position: ', end='')
+for pmin, vmin in enumerate(val):
+    if vmin == min(val):
+        print(pmin+1, end='...')
--- CursoemVideo/Exercicios Mundo 3/ex083_validando_expressoes_matematicas.py
+++ CursoemVideo/Exercicios Mundo 3/ex083_validando_expressoes_matematicas.py
@@ -3,3 +3,4 @@
 # Digitar uma expressao matemática qualquer
 # Verificar se os parenteses que abrem se fecham não pode faltar e nem ter a mais
 # Usar em forma de lista.
+
