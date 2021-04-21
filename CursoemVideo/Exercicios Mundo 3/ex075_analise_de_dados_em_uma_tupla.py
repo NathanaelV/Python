ff8410c0e3f68c0fffd4a2f9f2f65d8e2f949fa7
NathanaelV <tmntsplinter04@gmail.com>
2021-04-20 22:50:20 -0300

    Ex 75

--- CursoemVideo/Exercicios Mundo 3/ex075_analise_de_dados_em_uma_tupla.py
+++ CursoemVideo/Exercicios Mundo 3/ex075_analise_de_dados_em_uma_tupla.py
@@ -1,4 +1,5 @@
 # Class Challenge 16
+
 # Ler 4 números pelo teclado:
 # Colocar esses valores em um tupla e mostrar a tupla
 # Quantas vezes o valor 9 aparece
@@ -6,3 +7,22 @@
 # Quais são os pares digitados
 # Caso não tenha o número, falar que apareceu 0 vezes
 # Ou que não encontrou em nenhuma posição.
+
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+c = int(input('Enter third number: '))
+d = int(input('Enter fourth number: '))
+
+num = (a, b, c, d)
+print('-' * 30)
+print(f'The enter numbers are: {num}')
+print(f'The number 9 appears {num.count(9)} times.')
+if 3 in num:
+    print(f'The number 3 appears in position: {num.index(3)+1}')
+else:
+    print('No number 3 was entered.')
+print('Even number(s) is(are): ', end='')
+for c in range(0, 4):
+    if num[c] % 2 == 0:
+        print(num[c], end=' ')
+print('\n', '-' * 30)
