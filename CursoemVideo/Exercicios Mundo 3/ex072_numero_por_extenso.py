24697994fa671e449ffe4c4a092b6399d182d8eb
NathanaelV <tmntsplinter04@gmail.com>
2021-04-20 22:16:37 -0300

    Ex 73

--- CursoemVideo/Exercicios Mundo 3/ex072_numero_por_extenso.py
+++ CursoemVideo/Exercicios Mundo 3/ex072_numero_por_extenso.py
@@ -4,6 +4,7 @@
 # Usuario vai digita o valor e vai mostrar o valor por extendo.
 # Só é valido números entre 0 e 20:
 # um dois tres quatro
+
 count = ('Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve',
          'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen', 'Twenty')
 user = int(input('Enter a number between 0 and 20: '))
--- CursoemVideo/Exercicios Mundo 3/ex073_tuplas_com_times_de_futebol.py
+++ CursoemVideo/Exercicios Mundo 3/ex073_tuplas_com_times_de_futebol.py
@@ -8,3 +8,11 @@
 # Times: Corinthians, Palmeiras, Santos, Gremio, Cruzeiro, Flamengo, Vasco
 # Chapecoense, Atlético, Botafogo, Bahia, São Paulo, Fluminense, Esporte Recife,
 # EC Vitoria, Coritíba, Avaí, Ponte Preta e Atletico Goianiense
+
+teams = ('Corinthians', 'Palmeiras', 'Santos', 'Gremio', 'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense', 'Atlético',
+         'Botafogo', 'Bahia', 'São Paulo', 'Fluminense', 'Esporte Recife', 'EC Vitoria', 'Coritíba', 'Avaí',
+         'Ponte Preta', 'Atletico Goianiense')
+print(f'The first four: {teams[:4]}')
+print(f'The last four: {teams[-4:]}')
+print(f'The teams in alphabetic order: {sorted(teams)}')
+print('Chapecoense team show in {}ª position.'.format(teams.index('Chapecoense')+1))
