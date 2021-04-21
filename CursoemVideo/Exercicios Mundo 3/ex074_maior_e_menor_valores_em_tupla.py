8df9eb36b5702b24133494845be1cd8d753784f9
NathanaelV <tmntsplinter04@gmail.com>
2021-04-20 22:24:20 -0300

    Ex 74

--- CursoemVideo/Exercicios Mundo 3/ex074_maior_e_menor_valores_em_tupla.py
+++ CursoemVideo/Exercicios Mundo 3/ex074_maior_e_menor_valores_em_tupla.py
@@ -3,3 +3,10 @@
 # Gerar 5 números aleatórios e colocar em uma tupla
 # Mostrar a lista dos 5 números
 # Mostrar o maior e o menor da Tupla
+
+from random import randint
+num = (randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9))
+print(num)
+order = sorted(num)
+print(f'The largest number is {order[len(order)-1]}')
+print(f'The smallest number is {order[0]}')
