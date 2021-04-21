e8c7e9ca5c31934c91d3e10a342b17dbb8bfe0ba
NathanaelV <tmntsplinter04@gmail.com>
2021-04-20 21:38:56 -0300

    Ex 72

--- CursoemVideo/Exercicios Mundo 3/ex072_numero_por_extenso.py
+++ CursoemVideo/Exercicios Mundo 3/ex072_numero_por_extenso.py
@@ -3,4 +3,10 @@
 # Uma tupla com contagem até 20 por extenso.
 # Usuario vai digita o valor e vai mostrar o valor por extendo.
 # Só é valido números entre 0 e 20:
-
+# um dois tres quatro
+count = ('Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve',
+         'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen', 'Twenty')
+user = int(input('Enter a number between 0 and 20: '))
+while not 0 <= user <= 20:
+    user = int(input('Invalid! Please enter a number between 0 and 20: '))
+print(f'The number {user} if full is: {count[user]}')
