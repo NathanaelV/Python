9fe8029342972ab50b38fe6769c6150354b5f6a8
NathanaelV <tmntsplinter04@gmail.com>
2021-04-20 23:53:44 -0300

    Ex 77

--- CursoemVideo/Exercicios Mundo 3/ex077_contando_vogais_em_tupla.py
+++ CursoemVideo/Exercicios Mundo 3/ex077_contando_vogais_em_tupla.py
@@ -6,3 +6,20 @@
 # Ex.: CASA tem a a. PEDREIRO tem e e i 0
 # Lista: APRENDER, PROGRAMAR, LINGUAGEM, PYTHON, CURSOS, GRATIS, ESTUDAR, PRATICAR
 # TRABALHAR, MERCADO, PROGRAMADOR, FUTURO.
+
+words = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSOS', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR',
+         'MERCADO', 'PROGRAMADOR', 'FUTURO')
+a = 'APRENDERA'
+for w in words:
+    print(f'The word {w} has the vowels: ', end=' ')
+    if 'A' in w:
+        print('{}'.format(w[w.index('A')]), end=' ')
+    if 'E' in w:
+        print('{}'.format(w[w.index('E')]), end=' ')
+    if 'I' in w:
+        print('{}'.format(w[w.index('I')]), end=' ')
+    if 'O' in w:
+        print('{}'.format(w[w.index('O')]), end=' ')
+    if 'U' in w:
+        print('{}'.format(w[w.index('U')]), end=' ')
+    print('')
