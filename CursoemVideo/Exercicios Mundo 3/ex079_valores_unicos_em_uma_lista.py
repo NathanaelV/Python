8530586cfa6a34b1b518866e1c83da05ebcf8100
NathanaelV <tmntsplinter04@gmail.com>
2021-04-21 14:51:43 -0300

    Exercises 79

--- CursoemVideo/Exercicios Mundo 3/ex079_valores_unicos_em_uma_lista.py
+++ CursoemVideo/Exercicios Mundo 3/ex079_valores_unicos_em_uma_lista.py
@@ -1,7 +1,28 @@
 # Class Challenge 17
 
 # Digitar vários valores númericos, perguntando se quer continuar
-# Não adicionar números repetidos
+# Adicionar os números em uma lista. Não adicionar números repetidos
 # Notificar se o valor foi adicionado ou não
 # Exibir os valores em ordem crescente
 
+group = list()
+while True:
+    print('-' * 30)
+    num = int(input('Enter a numer: '))
+
+    if num not in group:
+        print('Number added to list...')
+        group.append(num)
+    else:
+        print('This number has already been added to the list...')
+
+    resp = str(input('Do you want to continue? [Y/N] ')).strip().upper()[0]
+    while resp not in 'YN':
+        print('\nInvalid response!')
+        resp = str(input('Do you want to continue? [Y/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+
+print('-'*30)
+group.sort()
+print(f'All values entered: {group}')
