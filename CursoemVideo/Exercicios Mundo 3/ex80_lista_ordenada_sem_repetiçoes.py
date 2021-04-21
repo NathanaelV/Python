9e299c2b3baa8113dee41567079fc502d108f5f6
NathanaelV <tmntsplinter04@gmail.com>
2021-04-21 17:02:48 -0300

    Exercises 81

--- CursoemVideo/Exercicios Mundo 3/ex80_lista_ordenada_sem_repetiçoes.py
+++ CursoemVideo/Exercicios Mundo 3/ex80_lista_ordenada_sem_repetiçoes.py
@@ -5,3 +5,18 @@
 # A cada valor adicionado, mostrar em qual posição foi adicionado
 # Ou está no primeiro ou último
 # Mostrar a lista com os valores em ordem
+
+order = list()
+for v in range(1, 6):
+    num = int(input(f'Enter {v}º number: '))
+    # if v == 1:
+    # print('First value added to the list.')
+    order.append(num)
+    for n in order:
+        if num > n:
+            order.append(num)
+        if num < n:
+            order.insert(0, num)
+    print(order)
+
+print(order)
--- CursoemVideo/Exercicios Mundo 3/ex81_extraindo_dados_de_uma_lista.py
+++ CursoemVideo/Exercicios Mundo 3/ex81_extraindo_dados_de_uma_lista.py
@@ -4,3 +4,27 @@
 # Mostrar quantos valores foram digitados
 # Mostrar a Lista de valores em ordem decrescente
 # Se o cinco foi digitado ou não e em qual posição.
+
+value = list()
+i = 0
+while True:
+    value.append(int(input('Enter a number: ')))
+    i += 1
+    resp = ' '
+    while resp not in 'yn':
+        resp = str(input('Do you want to continue? [Y/N] ')).strip().lower()[0]
+
+    if resp == 'n':
+        print('-' * 30)
+        break
+
+print(f'{i} values were entered.')
+if 5 in value:
+    print(f'The number 5 appeared in the positions: ', end='')
+    for pos, v in enumerate(value):
+        if v == 5:
+            print(pos+1, end=', ')
+else:
+    print('There is no number 5 in the list.', end='')
+value.sort(reverse=True)
+print(f'\nIn ascending order: {value}')
