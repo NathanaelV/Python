42de5ece18b0da2af3d7637f40b42cb190309ea8
NathanaelV <tmntsplinter04@gmail.com>
2021-04-22 12:31:38 -0300

    Exercises 81 by professor

--- CursoemVideo/Exercicios Mundo 2/ex037_conversor_de_bases_numericas.py
+++ CursoemVideo/Exercicios Mundo 2/ex037_conversor_de_bases_numericas.py
@@ -1,16 +1,22 @@
 # Class Challenge 12
 
 # Digitar um número e fazer a conversão para uma das opções:
-# 1 - Binário; 2 - Octal; 3 - Hexadecimal
-print('\033[1;32mEsse programa converte um número inteiro em uma das bases:\n'
-      '\033[1;35m    1 - Binário\n\033[1;36m    2 - Octal\n\033[1;32m    3 - Hexadecimal\033[m')
-base = int(input('Deseja converter um número para qual base?: '))
-número = int(input('Digite o número que deseja converte: '))
-if base == 1:
-      print('O número {} convertido para Binário é:\n{}'.format(número, bin(número)[2:]))
-elif base == 2:
-      print('O número {} convertido para Octal é:\n{}'.format(número, oct(número)[2:]))
-elif base == 3:
-      print('O número {} convertido para Hexadecimal é:\n{}'.format(número, hex(número)[2:]))
-else:
-      print('Por favor, digite uma opção válida. 1, 2 ou 3.')
+# 1 - tBinário; 2 - Octal; 3 - Hexadecimal
+
+while True:
+      print('\033[1;32mEsse programa converte um número inteiro em uma das bases:\n'
+            '\033[1;35m    1 - Binário\n\033[1;36m    2 - Octal\n\033[1;32m    3 - Hexadecimal\033[m\n'
+            '    4 - SAIR')
+      base = int(input('Deseja converter um número para qual base?: '))
+      if 1 <= base <= 4:
+            if base == 4:
+                  break
+            número = int(input('Digite o número que deseja converte: '))
+            if base == 1:
+                  print(f'O número {número} convertido para Binário é: {bin(número)[2:]}')
+            elif base == 2:
+                  print('O número {} convertido para Octal é: {}'.format(número, oct(número)[2:]))
+            elif base == 3:
+                  print('O número {} convertido para Hexadecimal é: {}'.format(número, hex(número)[2:].upper()))
+      else:
+            print('Por favor, digite uma opção válida. 1, 2, 3 ou 4.')
--- CursoemVideo/Exercicios Mundo 3/ex81_extraindo_dados_de_uma_lista.py
+++ CursoemVideo/Exercicios Mundo 3/ex81_extraindo_dados_de_uma_lista.py
@@ -28,3 +28,21 @@ else:
     print('There is no number 5 in the list.', end='')
 value.sort(reverse=True)
 print(f'\nIn ascending order: {value}')
+
+# Teacher Example:
+
+print('\nTeacher example:')
+valores = []
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('=-' * 30)
+print(f'Você digitou {len(valores)} elementos.')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são: {valores}')
+if 5 in valores:
+    print('O valor 5 faz parte da lista.')
+else:
+    print('O valor 5 não faz parte da lista.')
