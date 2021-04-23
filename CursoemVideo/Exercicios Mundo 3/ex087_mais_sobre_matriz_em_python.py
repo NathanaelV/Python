15f4cd45a6141f5a85af1a459580c003d07b3d88
NathanaelV <tmntsplinter04@gmail.com>
2021-04-23 12:05:32 -0300

    Exercise 87

--- CursoemVideo/Exercicios Mundo 3/ex087_mais_sobre_matriz_em_python.py
+++ CursoemVideo/Exercicios Mundo 3/ex087_mais_sobre_matriz_em_python.py
@@ -14,7 +14,23 @@ for m in range(0, 3):
     inter.append(int(input(f'Enter a number for Matrix [{m}][2]: ')))
     matrix.append(inter[:])
     inter.clear()
-print('=+=' * 30)
+
+print('=+=' * 15)
 print(f'[ {matrix[0][0]} ][ {matrix[0][1]} ][ {matrix[0][2]} ]')
-print(f'[ {matrix[1][0]} ][ {matrix[1][1]} [] {matrix[1][2]} ]')
-print(f'[ {matrix[2][0]}')
+print(f'[ {matrix[1][0]} ][ {matrix[1][1]} ][ {matrix[1][2]} ]')
+print(f'[ {matrix[2][0]} ][ {matrix[2][1]} ][ {matrix[2][2]} ]')
+print('=+=' * 15)
+
+even = 0
+for m in matrix:
+    for a in range(0, 3):
+        if m[a] % 2 == 0:
+            even += m[a]
+print(f'The sum of all even numbers is {even}')
+
+column = 0
+for m in matrix:
+    column += m[2]
+print(f'The sum of all third column is: {column}')
+
+print(f'The highest value in the second row: {max(matrix[1])}')
