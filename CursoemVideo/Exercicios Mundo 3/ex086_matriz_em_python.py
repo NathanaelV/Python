c8b9d9b84a42c9fb394e271ced98550158a66a83
NathanaelV <tmntsplinter04@gmail.com>
2021-04-23 11:23:55 -0300

    Exercise 86

--- CursoemVideo/Exercicios Mundo 3/ex086_matriz_em_python.py
+++ CursoemVideo/Exercicios Mundo 3/ex086_matriz_em_python.py
@@ -4,3 +4,15 @@
 # Shows the matrix with the correct formatting.
 # Using a single list with other lists.
 
+matrix = list()
+inter = list()
+for m in range(0, 3):
+    inter.append(int(input(f'Enter [{m}][0] number: ')))
+    inter.append(int(input(f'Enter [{m}][1] number: ')))
+    inter.append(int(input(f'Enter [{m}][2] number: ')))
+    matrix.append(inter[:])
+    inter.clear()
+print('=+=' * 20)
+print(f'[ {matrix[0][0]} ][ {matrix[0][1]} ][ {matrix[0][2]} ]')
+print(f'[ {matrix[1][0]} ][ {matrix[1][1]} ][ {matrix[1][2]} ]')
+print(f'[ {matrix[2][0]} ][ {matrix[2][1]} ][ {matrix[2][2]} ]')
--- CursoemVideo/Exercicios Mundo 3/ex087_mais_sobre_matriz_em_python.py
+++ CursoemVideo/Exercicios Mundo 3/ex087_mais_sobre_matriz_em_python.py
@@ -5,3 +5,16 @@
 # Show the sum of the third column.
 # Show the highest value in the second row.
 
+inter = list()
+matrix = list()
+
+for m in range(0, 3):
+    inter.append(int(input(f'Enter a number for Matrix [{m}][0]: ')))
+    inter.append(int(input(f'Enter a number for Matrix [{m}][1]: ')))
+    inter.append(int(input(f'Enter a number for Matrix [{m}][2]: ')))
+    matrix.append(inter[:])
+    inter.clear()
+print('=+=' * 30)
+print(f'[ {matrix[0][0]} ][ {matrix[0][1]} ][ {matrix[0][2]} ]')
+print(f'[ {matrix[1][0]} ][ {matrix[1][1]} [] {matrix[1][2]} ]')
+print(f'[ {matrix[2][0]}')
