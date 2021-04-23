e8d749acf36a22fcdd235c162d93b60bc8b966e5
NathanaelV <tmntsplinter04@gmail.com>
2021-04-23 20:38:01 -0300

    Exercise 88

--- CursoemVideo/Exercicios Mundo 3/ex088_palpites_para_a_mega_sena.py
+++ CursoemVideo/Exercicios Mundo 3/ex088_palpites_para_a_mega_sena.py
@@ -5,3 +5,31 @@
 # In a interval between 1 and 60. Numbers can't be repeated
 # Show numbers in ascending order.
 # To use sleep(1)
+
+from random import randint
+from time import sleep
+
+luck = list()
+mega = list()
+print('=' * 30)
+print(f'{"MEGA SENNA!":^30}')
+print('=' * 30)
+
+guess = int(input('How many guesses would you like? '))
+for a in range(0, guess):
+    while True:
+        num = randint(1, 60)
+        if num not in luck:
+            luck.append(num)
+        if len(luck) == 6:
+            break
+    luck.sort()
+    mega.append(luck[:])
+    luck.clear()
+
+print(f'\n=-=-=-=-= Drawing {guess} Games =-=-=-=-=')
+sleep(0.5)
+for n, m in enumerate(mega):
+    print(f'Game {n+1}: {m}')
+    sleep(0.5)
+print(f'{" < GOOD LUCK! > ":=^35}')
