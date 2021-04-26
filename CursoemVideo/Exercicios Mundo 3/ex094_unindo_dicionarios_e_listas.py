41be69c5248e5a6ab92af318937ff7b63fc4662b
NathanaelV <tmntsplinter04@gmail.com>
2021-04-25 21:48:52 -0300

    Teacher examples

--- CursoemVideo/Exercicios Mundo 3/ex094_unindo_dicionarios_e_listas.py
+++ CursoemVideo/Exercicios Mundo 3/ex094_unindo_dicionarios_e_listas.py
@@ -14,12 +14,16 @@ people = []
 while True:
     person['name'] = str(input('Name: ')).strip()
     person['gender'] = str(input('Gender [M/F]: ')).strip().upper()[0]
+    while person['gender'] not in 'MF':
+        print('ERROR! Please enter M or F: ')
+        person['gender'] = str(input('Gender [M/F]: ')).strip().upper()[0]
     person['age'] = int(input('Age: '))
     people.append(person.copy())
-    
-    resp = ' '
+
+    resp = str(input('Do you want to continue? [Y/N] '))
     while resp not in 'NnYy':
-        resp = str(input('Do you want to continue: ')).strip()[0]
+        print('ERROR! Please enter Y or N.')
+        resp = str(input('Do you want to continue? [Y/N] ')).strip()[0]
     if resp in 'Nn':
         break
 
@@ -28,22 +32,22 @@ person['total'] = 0
 for a in people:
     person['total'] += a['age']
 media = person['total'] / len(people)
-print(f'There are {len(people)} people registered')
-print(f'Media of the ages are: {media:.1f} years')
-print('Women in the list are: ', end='')
+print(f'A) There are {len(people)} people registered')
+print(f'B) Media of the ages are: {media:.1f} years')
+print('C) Women in the list are: ', end='')
 
-for n, m in enumerate(people):
+for m in people:
     if m['gender'] == 'F':
         print(f'{m["name"]}', end=', ')
 print()
-print('People who are above average age:')
+print('D) People who are above average age:')
 
-for n, m in enumerate(people):
+for m in people:
     if m['age'] > media:
-        print('')
+        print('   - ', end='')
         for k, v in m.items():
             print(f'{k.capitalize()} = {v}', end='; ')
-        # print('')
+        print('')
 print('\n<< ENCERRADO >>')
 
 # Mostrando com , 'e' e ponto final:
@@ -64,3 +68,50 @@ for n, m in enumerate(velho):
         print(m, end=' and ')
     else:  # m['age'] > person['total'] / len(people):
         print(f"{m}.")
+
+
+# Teacher Example:
+
+print('\nTeacher Example:')
+
+pessoa = dict()
+galera = list()
+soma = media = 0
+
+while True:
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    pessoa.clear()
+
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N')
+    if resp == 'N':
+        break
+
+print('-=' * 30)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+media = soma / len(galera)
+print(f'B) A média de idade é de {media:.2f} anos')
+print('C) As mulheres cadastradas foram: ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]}', end=', ')
+print()
+print('D) Lista das pessoas que estão acima da média:')
+for p in galera:
+    if p['idade'] >= media:
+        print('    ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v};', end='')
+        print()
+print(' << ENCERRADO >>')
--- CursoemVideo/Exercicios Mundo 3/ex095_aprimorando_os_dicionarios.py
+++ CursoemVideo/Exercicios Mundo 3/ex095_aprimorando_os_dicionarios.py
@@ -7,12 +7,7 @@
 #    Mostra os jogos e quantos gol marcados nesses jogos
 #    999 para sair do Menu
 
-team = [{'name': 'Alan F.', 'goal': [2, 0]}, {'name': 'Rogerio M.', 'goal': [1]}, {'name': 'Azanha M.', 'goal': [0, 0]},
-        {'name': 'Pedro V.', 'goal': [2, 1, 2, 4]}, {'name': 'Joaquim S.', 'goal': []},
-        {'name': 'Nathan C.', 'goal': [5, 6, 5, 3]}, {'name': 'Cristiano R.', 'goal': [0, 2, 1]},
-        {'name': 'Joao Judas', 'goal': [2, 1, -3]}]
-
-'''
+team = []
 player = dict()
 goal = list()
 
@@ -31,13 +26,12 @@ while True:
         resp = str(input('Do you want to continue? [Y/N] ')).strip()[0]
     if resp in 'Nn':
         break
-'''
 
 print('-=' * 30)
-print(f'{"Nª":<4}{"Name":<15}{"Total":<6}{"Goals":<15}')
+print(f'{"Nª":<4}{"Name":<15}{"Goals":<15}{"Total":>6}')
 print('-' * 50)
 for n, d in enumerate(team):
-    print(f'{n+1:<3}{d["name"]:<15}{sum(d["goal"]):<6}{d["goal"]}')
+    print(f'{n+1:<4}{d["name"]:<15}{str(d["goal"]):<15}{sum(d["goal"]):>6}')
 print('-=' * 30)
 
 while True:
@@ -55,3 +49,56 @@ while True:
         print('  <<< CHECK BACK OFTEN! >>>')
         break
     print('-' * 60)
+
+# Teacher Example:
+
+print('\nTeacher Example')
+
+team = []
+player = dict()
+goal = list()
+
+while True:
+    print('-' * 30)
+    player['name'] = str(input('Soccer player name: '))
+    matches = int(input(f'How many matches {player["name"]} played? '))
+    for c in range(0, matches):
+        goal.append(int(input(f'How many goals in {c+1}º game? ')))
+    player['goal'] = goal[:]
+    player['total'] = sum(goal)
+    goal.clear()
+    team.append(player.copy())
+
+    while True:
+        resp = str(input('Do you want to continue? [Y/N] ')).strip().upper()[0]
+        if resp in 'SN':
+            break
+        print('ERROR! Answer only Y or N.')
+    if resp in 'Nn':
+        break
+
+print('-=' * 30)
+print('Cod ', end='')
+for i in player.keys():
+    print(f'{i.upper():<15}', end='')
+print()
+print('-' * 40)
+for k, v in enumerate(team):
+    print(f'{k:<4}', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
+
+while True:
+    busca = int(input('Mostrar dados de qual Jogador? (999 para sair) '))
+    if busca == 999:
+        break
+    if busca >= len(team):
+        print(f'ERROR! Não existe jogador de código {busca}')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {team[busca]["name"]}')
+        for i, g in enumerate(team[busca]["goal"]):
+            print(f'    No jogo {i+1} fez {g} gols.')
+    print('-' * 30)
+print(' <<< VOLTE SEMPRE >>>')
