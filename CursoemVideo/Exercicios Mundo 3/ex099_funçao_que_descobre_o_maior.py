8a5ced1c651d0f02d2cd836d20431f8fea656d7b
NathanaelV <tmntsplinter04@gmail.com>
2021-04-26 10:31:34 -0300

    Update exercises 96 - 100 exercise 100 done

--- CursoemVideo/Exercicios Mundo 3/ex099_funçao_que_descobre_o_maior.py
+++ CursoemVideo/Exercicios Mundo 3/ex099_funçao_que_descobre_o_maior.py
@@ -4,3 +4,40 @@
 # Deve mostrar qual é o maior.
 # Falar quais foram os maiores valores
 
+
+def maior(*num):
+    print('-=' * 30)
+    big = 0
+    print('Analyzing past values...')
+    for a, n in enumerate(num):
+        print(n, end=' ')
+        if a == 0:
+            big = n
+        elif n > big:
+            big = n
+    print(f':{len(num)} values were entered in all.')
+    print(f'The highest value is {big}')
+
+
+def tit(msg):
+    t = len(msg)//2 + 2
+    print('-=' * t)
+    print(f'  {msg.title()}')
+    print('-=' * t)
+
+
+tit('Calculating the highest!')
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(2, 1)
+maior(6)
+maior()
+
+''''''
+print('-=' * 30)
+value = list()
+p = int(input('How many values do you want to analyze? '))
+for c in range(0, p):
+    value.append(int(input('Enter a number: ')))
+maior(value)
--- CursoemVideo/Exercicios Mundo 3/ex100_funções_para_sortear_e_somar.py
+++ CursoemVideo/Exercicios Mundo 3/ex100_funções_para_sortear_e_somar.py
@@ -3,3 +3,31 @@
 # lista com números.
 # Criar uma função sorteia. Sorteia 5 números e coloca na lista
 # Criar uma função somaPar(). pega a funão anterior e soma só os pares
+
+from random import randint
+from time import sleep
+
+
+def sorteia(lst):
+    print('Os números sorteados são: ', end='')
+    for c in range(0, 5):
+        sort = randint(0, 9)
+        print(sort, end=' ')
+        sleep(0.3)
+        lst.append(sort)
+    print('PRONTO!')
+
+
+def somapar(lst):
+    print(f'Os pares são (', end=' ')
+    s = 0
+    for a, n in enumerate(lst):
+        if n % 2 == 0:
+            print(n, end=' ')
+            s += n
+    print(f'). A soma dos pares é {s}.')
+
+
+num = list()
+sorteia(num)
+somapar(num)
